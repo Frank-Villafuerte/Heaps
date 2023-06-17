dadd9c773cc8d69f2221ed37fc9d1b305e5adac6
Frank Villafuerte <133295276+Frank-Villafuerte@users.noreply.github.com>
2023-06-17 02:02:28 -0500

    Clase principal con Cola de prioridad

--- principal/Test.java
+++ principal/Test.java
@@ -1,19 +1,33 @@
 package principal;
 import heap.HeapTree;
+import colaPrioridad.PriorityQueueHeap;
 import myExceptions.ExceptionIsEmpty;
 public class Test {
 
 	public static void main(String[] args) throws ExceptionIsEmpty {
 		
-		HeapTree arbol=new HeapTree();
+		
+		PriorityQueueHeap<String> colaPrioridad=new PriorityQueueHeap<String>();
+		colaPrioridad.enqueue("p=1", 1);
+		colaPrioridad.enqueue("p=5", 5);
+		colaPrioridad.enqueue("p=2", 2);
+		colaPrioridad.enqueue("p=9", 9);
+		colaPrioridad.enqueue("p=8", 8);
+		colaPrioridad.enqueue("p=4", 4);
+		colaPrioridad.enqueue("p=3", 3);
+		colaPrioridad.enqueue("p=15", 15);
+		System.out.println(colaPrioridad);
+		colaPrioridad.dequeue();
+		
+		System.out.println(colaPrioridad);
+		/*HeapTree<Integer> arbol=new HeapTree<Integer>();
 		arbol.insert(7);
 		p(arbol);
 		arbol.insert(4);
 		p(arbol);
 		arbol.insert(8);
 		p(arbol);
-		arbol.insert(15);
-		p(arbol);
+		
 		arbol.insert(16);
 		p(arbol);
 		arbol.insert(9);
@@ -24,14 +38,14 @@ public class Test {
 		p(arbol);
 		arbol.insert(12);
 		p(arbol);
-		/*arbol.insert(1);
-		p(arbol);*/
-		/*arbol.insert(15);
+		arbol.insert(1);
+		p(arbol);
+		arbol.insert(15);
 		p(arbol);
 		
-		p(arbol);*/
-		arbol.eliminar();
 		p(arbol);
+		arbol.eliminar();
+		p(arbol);*/
 	}
 	public static void p(HeapTree arbol) {
 		
